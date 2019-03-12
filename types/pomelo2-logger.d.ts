0cd44c64dce7aff85499349a4fcc50e151e9adc9
Aslijia <medivh.wd@gamail.com>
2019-03-12 16:27:33 +0800

    update typedefine for logger.configure

--- types/pomelo2-logger.d.ts
+++ types/pomelo2-logger.d.ts
@@ -1,5 +1,5 @@
 import { Logger } from "log4js";
 
-export function configure(config: string | object, opts: object);
+export function configure(config: string | object, opts: object): any;
 
 export function getLogger(categoryName: string, ...prefix: string[]): Logger;
