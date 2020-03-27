e9cf39b589f6e2980e20123c4a0479731c212334
Aslijia <medivh.wd@gamail.com>
2020-03-27 21:25:05 +0800

    调整日志格式

--- types/pomelo2-logger.d.ts
+++ types/pomelo2-logger.d.ts
@@ -2,4 +2,4 @@ import { Logger } from "log4js";
 
 export function configure(config: string | object, opts: object): any;
 
-export function getLogger(categoryName: string, ...prefix: string[]): Logger;
+export function getLogger(categoryName: string): Logger;
